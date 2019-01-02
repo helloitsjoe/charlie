aa1f493a403c1c3ed0d3c3bac40e704a450ca7f0
helloitsjoe <jboyle617@gmail.com>
2019-01-01 23:55:54 -0500

    Change format of mbta response to frontend

--- main/index.js
+++ main/index.js
@@ -17,32 +17,30 @@ try {
 }
 
 const PREDICTIONS_LIMIT = 4;
-const CACHE_TTL = 60 * 1000;
+// const CACHE_TTL = 60 * 1000;
 const assetsDir = path.join(__dirname, '../assets');
-const cache = new Map();
+// const cache = new Map();
 const mbta = new MBTA(mbtaKey);
 
 let tray;
 let window;
 let timeout;
 const routes = [backBayOrange, backBayCR, southStation, clearway];
-let currentIndex = 0;
 
 app.on('ready', () => {
   tray = new Tray(path.join(assetsDir, 'mbta-logo-black.png'));
   window = new BrowserWindow({
-    width: 260,
+    width: 340,
     height: 400,
     show: false,
     frame: false,
     resizable: false,
   });
 
-  fetchAndSend(routes[currentIndex]);
+  fetchAndSend(routes);
 
   tray.on('double-click', () => window.openDevTools({ mode: 'detach' }));
   tray.on('click', event => {
-    fetchAndSend(routes[currentIndex]);
     toggleWindow();
   });
 
@@ -50,14 +48,7 @@ app.on('ready', () => {
   window.on('blur', window.hide);
 
   ipcMain.on('fetch', (sender, data) => {
-    // const newIndex =
-    //   data && data.key === 'ArrowLeft'
-    //     ? getPrevIndex(routes, currentIndex)
-    //     : getNextIndex(routes, currentIndex);
-
-    // currentIndex = newIndex;
-    // fetchAndSend(routes[newIndex]);
-    fetchAndSend();
+    fetchAndSend(routes);
   });
 
   ipcMain.on('change-icon', (sender, data) => {
@@ -70,64 +61,62 @@ app.on('ready', () => {
   });
 });
 
-// const getNextIndex = (arr, i) => (i < arr.length - 1 ? i + 1 : 0);
-// const getPrevIndex = (arr, i) => (i < 1 ? arr.length - 1 : i - 1);
-
-const fetchAndSend = route => {
+const fetchAndSend = routes => {
   clearTimeout(timeout);
 
-  return fetchData(route).then(data => {
+  return fetchData(routes).then(predictions => {
     // Send update event to browser window
-    window.webContents.send('update', { ...data /* , route */ });
+    window.webContents.send('update', predictions);
 
     // Kick off check every 60 seconds
     timeout = setTimeout(() => {
-      fetchAndSend(route);
+      fetchAndSend(routes);
     }, 60 * 1000);
   });
 };
 
-const fetchData = route => {
-  // const cached = cache.get(route.name);
-  // const withinTTL = cached && Date.now() - cached.ts < CACHE_TTL;
+const fetchData = async routes => {
+  const predictionPromises = Promise.all(
+    routes.map(route =>
+      mbta.fetchPredictions({
+        limit: PREDICTIONS_LIMIT,
+        stop: route.code,
+        direction_id: route.direction,
+        sort: 'arrival_time',
+        include: ['stop', 'route'],
+      })
+    )
+  );
 
-  // Use cache if within TTL, otherwise fetch live data.
-  // Click to your heart's delight and this will only fetch once a minute
-  // if (withinTTL) return Promise.resolve(cached);
+  try {
+    const predictions = await predictionPromises;
+    console.log(`Fetched live data`);
 
-  return mbta
-    .fetchPredictions({
-      // limit: 4,
-      stop: routes.map(route => route.code),
-      // direction_id: route.direction,
-      sort: 'arrival_time',
-    })
-    .then(result => {
-      console.log(`Fetched live data`);
-      const minsToArrival = mbta.selectArrivals(result, { convertTo: 'min' });
-      const extra = {
-        ts: Date.now(),
-        minsToArrival: minsToArrival.filter(arrival => arrival > 2),
+    return predictions.map(pre => {
+      const arrivals = mbta.selectArrivals(pre, { convertTo: 'min' });
+      const stopName = mbta.selectIncluded(pre, 'stop')[0].attributes.name;
+      const routeAttrs = mbta.selectIncluded(pre, 'route')[0].attributes;
+      const directionIdx = pre.data[0].attributes.direction_id;
+      const direction =
+        routeAttrs.direction_destinations[directionIdx] ||
+        routeAttrs.direction_names[directionIdx];
+
+      const color = routeAttrs.color;
+      const textColor = routeAttrs.text_color;
+
+      return {
+        color,
+        stopName,
+        direction,
+        textColor,
+        arrivalMins: arrivals.filter(min => min > 2),
+        _prediction: pre,
       };
-      // const cachedPrediction = { ...result, ...extra };
-      // cache.set(route.name, cachedPrediction);
-      // return cachedPrediction;
-
-      // Create object of prediction arrays based on ID
-      return result.data.reduce((predictions, curr) => {
-        const { id: stopID } = curr.relationships.stop.data;
-        if (predictions[stopID] == null) {
-          predictions[stopID] = [curr];
-        } else if (predictions[stopID].length < PREDICTIONS_LIMIT) {
-          predictions[stopID].push(curr);
-        }
-        return predictions;
-      }, {});
-    })
-    .catch(err => {
-      console.error('Error during fetch:', err.message);
-      return null;
     });
+  } catch (err) {
+    console.error('Error during fetch:', err.message);
+    return null;
+  }
 };
 
 const toggleWindow = () => {
