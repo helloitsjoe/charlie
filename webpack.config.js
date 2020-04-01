fa12f7ac147acaab6423b41f43d1c7f32e66d28e
helloitsjoe <jboyle617@gmail.com>
2020-04-01 06:34:22 -0400

    Bundle splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const { makeWebpackConfig } = require('webpack-simple');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -7,9 +8,31 @@ const htmlPlugin = new HtmlWebpackPlugin({
 });
 
 module.exports = makeWebpackConfig({
-  plugins: [htmlPlugin],
+  plugins: [htmlPlugin, new webpack.HashedModuleIdsPlugin()],
   output: {
-    filename: '[name].[contenthash].js',
+    // filename: '[name].[contenthash:8].js',
+    filename: '[name].js',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
   },
   resolve: {
     alias: {
