407c1d575dc564ac86bef4e8ec804ff4e195baf5
helloitsjoe <jboyle617@gmail.com>
2020-04-02 06:39:46 -0400

    change class to id in index

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-ReactDOM.render(<App />, document.querySelector('.main'));
+ReactDOM.render(<App />, document.getElementById('main'));
